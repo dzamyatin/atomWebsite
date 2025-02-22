692e21f79ac6069c87dc14e79a6d355460372b87
d.zamyatin <d.zamyatin@eapteka.ru>
2025-02-22 23:51:34 +0300

    fx

--- app/main.go
+++ app/main.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/dzamyatin/atomWebsite/internal/di"
 	"github.com/dzamyatin/atomWebsite/internal/service/arg"
 	"log"
 )
 
 func main() {
-	fmt.Println("Server starting...")
-
 	ctx := context.Background()
 
 	a := arg.NewArg()
 	err := di.CreateConfig(a.Config)
+
 	if err != nil {
 		log.Fatalf("failed to create config: %v", err)
 	}
@@ -25,6 +23,4 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to start: %v", err)
 	}
-
-	fmt.Println("Done")
 }
