3d5646169a6c67e834994dc4f218e3db242b6cc7
d.zamyatin <d.zamyatin@eapteka.ru>
2025-04-26 22:19:40 +0300

    fx

---
+++ frontend/README.md
@@ -0,0 +1,5 @@
+
+
+https://v3.buefy.org/
+https://github.com/ntohq/buefy-next
+https://bulma.io/documentation/
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,31 +6,12 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // presets: [
-  //   [
-  //     "@vue/app",
-  //     {
-  //       "polyfills": ["es7.object.entries", "es6.promise"]
-  //     }
-  //   ]
-  // ],
   plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          compatConfig: {
-            MODE: 2,
-          },
-        },
-      },
-    }),
+    vue(),
     vueDevTools(),
   ],
   resolve: {
-    // extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
-    extensions: ['.js','.vue'],
     alias: {
-      vue: '@vue/compat',
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
