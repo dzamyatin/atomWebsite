6839322a2ca7ee16b085ec97c9a8a11293619c04
d.zamyatin <d.zamyatin@eapteka.ru>
2025-06-11 09:46:01 +0300

    fx

--- app/internal/grpc/grpc/authserver.go
+++ app/internal/grpc/grpc/authserver.go
@@ -5,7 +5,6 @@ import (
 	atomWebsite "github.com/dzamyatin/atomWebsite/internal/grpc/generated"
 	"github.com/dzamyatin/atomWebsite/internal/request"
 	"github.com/dzamyatin/atomWebsite/internal/service/bus"
-	"github.com/dzamyatin/atomWebsite/internal/service/command"
 	"github.com/dzamyatin/atomWebsite/internal/usecase"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -31,23 +30,23 @@ func NewAuthServer(
 }
 
 func (r AuthServer) Register(ctx context.Context, req *atomWebsite.RegisterRequest) (*atomWebsite.RegisterResponse, error) {
-	//err := r.registerUseCase.Execute(
-	//	ctx,
-	//	request.RegistrationRequest{
-	//		Email:    req.Email,
-	//		Password: req.Password,
-	//		Phone:    req.Phone,
-	//	},
-	//)
-
-	err := r.bus.Dispatch(
+	err := r.registerUseCase.Execute(
 		ctx,
-		&command.RegisterCommand{Req: request.RegistrationRequest{
+		request.RegistrationRequest{
 			Email:    req.Email,
 			Password: req.Password,
 			Phone:    req.Phone,
-		}},
+		},
 	)
+
+	//err := r.bus.Dispatch(
+	//	ctx,
+	//	&command.RegisterCommand{Req: request.RegistrationRequest{
+	//		Email:    req.Email,
+	//		Password: req.Password,
+	//		Phone:    req.Phone,
+	//	}},
+	//)
 
 	if err != nil {
 		return nil, status.Errorf(codes.InvalidArgument, "Registration fail: %s", err.Error())
